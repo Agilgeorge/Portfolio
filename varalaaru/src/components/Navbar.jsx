50706beb94001f43b76a4172d5b7b41d2e66858e
Agilgeorge <111696090+Agilgeorge@users.noreply.github.com>
2024-11-17 00:05:23 +0530

    media content added

--- varalaaru/src/components/Navbar.jsx
+++ varalaaru/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-
+import wojak from "../assets/wojak.jpg"
+import logo from "../assets/logo.png"
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-center py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img src="" alt="" />
+            <img src={logo} alt="" />
 
         </div>
     </nav>
