10bc34118ad031790bcb13dd8c52cf25bd4250db
Agilgeorge <111696090+Agilgeorge@users.noreply.github.com>
2024-11-18 23:47:02 +0530

    changes in navbar

--- varalaaru/src/components/Navbar.jsx
+++ varalaaru/src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import wojak from "../assets/wojak.jpg"
+import {FaLinkedin} from "react-icons/fa";
+import {FaGithub} from "react-icons/fa";
+import {FaDiscord} from "react-icons/fa";
+ 
 import logo from "../assets/logo.png"
 const Navbar = () => {
   return (
-    <nav className="mb-20 flex items-center justify-center py-6">
+    <nav className="bg-black mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
             <img src={logo} alt="" />
-
+        </div>
+        <div className="m-8 flex items-center justify-center">
+          <FaLinkedin/>
+          <FaGithub/>
+          <FaDiscord/>
         </div>
     </nav>
   )
