23bcc3729754f6584f33962b1d20d1d74090ba0f
Agilgeorge <111696090+Agilgeorge@users.noreply.github.com>
2024-11-21 00:33:40 +0530

    main profile displayed

--- varalaaru/src/components/Navbar.jsx
+++ varalaaru/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <nav className=" mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img src={logo} className=" ml-3 h-44 w-44" alt="" />
+            <img src={logo} className=" ml-3 h-28 w-28" alt="" />
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-xl">
           <FaLinkedin/>
--- varalaaru/src/components/hero.jsx
+++ varalaaru/src/components/hero.jsx
@@ -1,16 +1,26 @@
+import wojak from "../assets/wojak.jpg";
 const Hero = () => {
     return (
       <div className="border-b border-neutral-900 pb-4 lg:mb-36">
           <div className="flex flex-wrap">
               <div className="w-full lg:w-1/2">
                 <div className="flex flex-col items-center lg:items-start">
-                  <h1>Akhil George</h1>
+                  <h1 className="pb-16 text-xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Akhil George</h1>
+                  <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-light text-transparent">Web and App developer</span>
+                  <p className="my-2 max-w-xl py-6 font-light tracking-lighter"> Aspiring Web and App Developer with a keen eye for design and a strong foundation in multiple
+                  programming languages and frameworks. Passionate about learning new technologies,collaborating with diverse teams, and driving innovation in development projects. Proven track
+                  record of success in competitive hackathons and internships.</p>
               </div>
             </div>
+             <div className="w-full lg:w-1/2 lg:p-8">
+             <div className="flex justify-center">
+                <img  className=" h-80 w-80  mt-4" src={wojak} alt="agil"/>
+             </div>
+
+             </div>
           </div>
       </div>
     );
   }
-  
   export default Hero;
   
