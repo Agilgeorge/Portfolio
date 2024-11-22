6e1ad5e49ab8cd6c4ef6165cd1cd6b03d06fbcd9
Agil george <agilavenger017@gmail.com>
2024-11-22 17:50:18 +0000

    slight change in color

--- varalaaru/src/components/Technology.jsx
+++ varalaaru/src/components/Technology.jsx
@@ -12,7 +12,7 @@ Tech Stack
         </h1>
         <div className="flex flex-wrap items-center justify-center gap-4">
             <div className="rounded-2xl border-4 border-neutral-800 p-4">
-                <RiReactjsLine className="text-7xl text-blue-800"/>
+                <RiReactjsLine className="text-7xl text-blue-500"/>
             </div>
             <div className="rounded-2xl border-4 border-neutral-800 p-4">
                 <SiMongodb className="text-7xl text-green-500"/>
