2ca2f7acecf89580990d071f63efc8a3d104e3e5
Agil george <agilavenger017@gmail.com>
2024-11-25 18:35:26 +0000

    site completed framer balance

--- varalaaru/src/components/contack.jsx
+++ varalaaru/src/components/contack.jsx
@@ -1,7 +1,14 @@
+import { CONTACT } from "../prebuilds";
+
 const Contact = ( ) => {
     return (
     <div className="border-b ☐ border-neutral-900 pb-20">
-    <h1 className="|">Contact</h1>
+    <h1 className="my-10 text-center text-4xl">whatsuup peeps!!</h1>
+    <div className="text-center trackking-tighter">
+        <p className="my-4">{CONTACT.address}</p>
+        <a href="#" className="border-b">{CONTACT.email}</a>
+
+    </div>
     </div>
     );
     };
