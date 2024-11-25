4606db103d2536fd9cb1059d7ff0fac1e7b8cf50
Agil george <agilavenger017@gmail.com>
2024-11-25 18:27:58 +0000

    code to contact

---
+++ varalaaru/src/components/contack.jsx
@@ -0,0 +1,8 @@
+const Contact = ( ) => {
+    return (
+    <div className="border-b ☐ border-neutral-900 pb-20">
+    <h1 className="|">Contact</h1>
+    </div>
+    );
+    };
+    export default Contact;
