dc335a171a09854c7b5d47ec6e607895835b981c
Agil george <agilavenger017@gmail.com>
2024-11-26 18:29:49 +0000

    eroor in hero resolved

--- varalaaru/src/components/hero.jsx
+++ varalaaru/src/components/hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
           <div className="flex flex-wrap">
               <div className="w-full lg:w-1/2">
                 <div className="flex flex-col items-center lg:items-start">
-                  <motion.h1 animate={{ x: 100 }} className="pb-16 text-xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Akhil George</h1>
+                  <motion.h1 animate={{ x: 100 }} className="pb-16 text-xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Akhil George</motion.h1>
                   <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-light text-transparent">Web and App developer</span>
                   <p className="my-2 max-w-xl py-6 font-light tracking-lighter"> Aspiring Web and App Developer with a keen eye for design and a strong foundation in multiple
                   programming languages and frameworks. Passionate about learning new technologies,collaborating with diverse teams, and driving innovation in development projects. Proven track
